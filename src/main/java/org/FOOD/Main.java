c774c320dae8629118714f9959c24782451fc1ec
Vidhatu Patel <62958219+Kemchobro@users.noreply.github.com>
2024-12-02 12:05:58 -0500

    update

--- src/main/java/org/FOOD/Main.java
+++ src/main/java/org/FOOD/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         System.out.println("What is your query?: ");
 
-    RequestHandler requestHandler = new RequestHandler(scanner.nextLine());
+    RequestHandler requestHandler = new RequestHandler(scanner.nextLine(), RequestHandler.RequestType.EVERYTHING);
 
 
 
--- src/main/java/org/FOOD/RequestHandler.java
+++ src/main/java/org/FOOD/RequestHandler.java
@@ -2,37 +2,215 @@ package org.FOOD;
 
 import org.kwabenaberko.newsapilib.NewsApiClient;
 import org.kwabenaberko.newsapilib.models.Article;
+import org.kwabenaberko.newsapilib.models.Source;
 import org.kwabenaberko.newsapilib.models.request.EverythingRequest;
+import org.kwabenaberko.newsapilib.models.request.SourcesRequest;
+import org.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
 import org.kwabenaberko.newsapilib.models.response.ArticleResponse;
+import org.kwabenaberko.newsapilib.models.response.SourcesResponse;
+
+import javax.xml.stream.events.Characters;
+import java.sql.Date;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.*;
+import java.util.Locale;
 
 public class RequestHandler {
 
     NewsApiClient newsApiClient = new NewsApiClient("5d3a947c1a7d4e9e8abea0ffa1f074d7");
 
-    public RequestHandler(String query) {
 
-        newsApiClient.getEverything(
-                new EverythingRequest.Builder()
-                        .q(query)
-                        .build(),
-                new NewsApiClient.ArticlesResponseCallback() {
-                    @Override
-                    public void onSuccess(ArticleResponse response) {
-                        for (Article article : response.getArticles()) {
-                            System.out.println(article.getTitle());
-                            System.out.println(article.getUrl());
-                            System.out.println(article.getDescription());
+    public RequestHandler(String query, RequestType requestType) {
+
+
+        switch (requestType) {
+            case EVERYTHING ->
+                newsApiClient.getEverything(
+                    new EverythingRequest.Builder()
+                            .q(query)
+                            .build(),
+                    new NewsApiClient.ArticlesResponseCallback() {
+                        @Override
+                        public void onSuccess(ArticleResponse response) {
+                            for (Article article : response.getArticles()) {
+                                System.out.println(article.getTitle());
+                                System.out.println(article.getUrl());
+                                System.out.println(article.getDescription());
+                                System.out.println(article.getContent());
+                                try {
+
+                                    // Getting the Date from String by
+                                    // creating object of Instant class
+                                    Instant timestamp = getDateFromString(article.getPublishedAt());
+
+                                    // Printing the converted date
+
+                                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                            .withZone(ZoneId.systemDefault());
+                                    String customFormatted = formatter.format(timestamp);
+                                    System.out.println("Date: " + customFormatted);
+
+                                }
+
+                                // Catch block to handle exceptions
+                                catch (DateTimeParseException e) {
+
+                                    // Throws DateTimeParseException
+                                    // if the string cannot be parsed
+                                    System.out.println("Exception: " + e);
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Throwable throwable) {
+                            System.out.println(throwable.getMessage());
                         }
                     }
+            );
 
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        System.out.println(throwable.getMessage());
+
+            case HEADLINES ->
+
+                    newsApiClient.getTopHeadlines(
+                            new TopHeadlinesRequest.Builder()
+                                    .q(query)
+                                    .build(),
+                            new NewsApiClient.ArticlesResponseCallback() {
+                                @Override
+                                public void onSuccess(ArticleResponse response) {
+                                    for (Article article : response.getArticles()) {
+                                        System.out.println(article.getTitle());
+                                        System.out.println(article.getUrl());
+                                        System.out.println(article.getDescription());
+                                        System.out.println(article.getContent());
+                                        try {
+
+                                            // Getting the Date from String by
+                                            // creating object of Instant class
+                                            Instant timestamp = getDateFromString(article.getPublishedAt());
+
+                                            // Printing the converted date
+
+                                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                                    .withZone(ZoneId.systemDefault());
+                                            String customFormatted = formatter.format(timestamp);
+                                            System.out.println("Date: " + customFormatted);
+
+                                        }
+
+                                        // Catch block to handle exceptions
+                                        catch (DateTimeParseException e) {
+
+                                            // Throws DateTimeParseException
+                                            // if the string cannot be parsed
+                                            System.out.println("Exception: " + e);
+                                        }
+                                    }
+                                }
+
+                                @Override
+                                public void onFailure(Throwable throwable) {
+                                    System.out.println(throwable.getMessage());
+                                }
+                            }
+                    );
+
+            case SOURCES ->    newsApiClient.getSources(
+                    new SourcesRequest.Builder()
+                            .category(query)
+                            .build(),
+                    new NewsApiClient.SourcesCallback() {
+                        @Override
+                        public void onSuccess(SourcesResponse response) {
+                            for (Source source : response.getSources()) {
+                                System.out.println(source.getUrl());
+                                System.out.println(source.getName());
+                                System.out.println(source.getDescription());
+                                System.out.println(source.getCategory());
+
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Throwable throwable) {
+                            System.out.println(throwable.getMessage());
+                        }
                     }
-                }
-        );
+            );
+
+            default ->    newsApiClient.getEverything(
+                    new EverythingRequest.Builder()
+                            .q(query)
+                            .build(),
+                    new NewsApiClient.ArticlesResponseCallback() {
+                        @Override
+                        public void onSuccess(ArticleResponse response) {
+                            for (Article article : response.getArticles()) {
+                                System.out.println(article.getTitle());
+                                System.out.println(article.getUrl());
+                                System.out.println(article.getDescription());
+                                System.out.println(article.getContent());
+                                try {
+
+                                    // Getting the Date from String by
+                                    // creating object of Instant class
+                                    Instant timestamp = getDateFromString(article.getPublishedAt());
+
+                                    // Printing the converted date
+
+                                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                                            .withZone(ZoneId.systemDefault());
+                                    String customFormatted = formatter.format(timestamp);
+                                    System.out.println("Date: " + customFormatted);
+
+                                }
+
+                                // Catch block to handle exceptions
+                                catch (DateTimeParseException e) {
+
+                                    // Throws DateTimeParseException
+                                    // if the string cannot be parsed
+                                    System.out.println("Exception: " + e);
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Throwable throwable) {
+                            System.out.println(throwable.getMessage());
+                        }
+                    }
+            );
+
+        }
 
 
     }
 
+    public enum RequestType {
+        EVERYTHING,
+        HEADLINES,
+        SOURCES
+    }
+
+    public static Instant getDateFromString(String string)
+    {
+        // Creating an instant object
+        Instant timestamp = null;
+
+        // Parsing the string to Date
+        timestamp = Instant.parse(string);
+
+        // Returning the converted timestamp
+        return timestamp;
+    }
+
+
 }
+
+
