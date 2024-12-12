01d3fec5e6a1fbfacfc9d389f801acd8693de771
Vidhatu Patel <62958219+Kemchobro@users.noreply.github.com>
2024-12-12 12:01:05 -0500

    reading files

--- src/main/java/org/FOOD/RecentHistory.java
+++ src/main/java/org/FOOD/RecentHistory.java
@@ -1,9 +1,14 @@
 package org.FOOD;
 
+import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RecentHistory {
 
@@ -15,6 +20,19 @@ public class RecentHistory {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<String[]> getStrings() {
+        String csvFile = "src/main/java/org/FOOD/RecentHistory.csv";
+        List<String[]> rows = new ArrayList<>();
+
+        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+            rows = reader.readAll();
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+
+        return rows;
     }
 
 
